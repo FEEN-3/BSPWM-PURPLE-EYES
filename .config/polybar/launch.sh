c3aa98ead3083f094c0db156b582bedcbf12e410
FEEN <daltonc3@tutanota.com>
2022-09-13 20:23:28 -0400

    First Commit 09-13-2022

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar top -c ~/.config/polybar/config.ini &
+
+if [[ $(xrandr -q | grep 'HDMI-A-0 connected') ]]; then
+    polybar top_external -c ~/.config/polybar/config.ini &
+fi
---
+++ .screenlayout/monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-A-0 --primary --mode 2560x1440 --rate 143.91 --rotate normal --output eDP --mode 1920x1080 --below HDMI-A-0 --rotate normal 
